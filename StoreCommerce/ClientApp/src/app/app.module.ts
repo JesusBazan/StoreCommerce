40976b8a2ff065290782141e02a5fcf801a737d9
EmmanuelBazan <mqkz99@gmail.com>
2020-09-17 23:23:21 -0500

    Creacion de componente provedores

--- StoreCommerce/ClientApp/src/app/app.module.ts
+++ StoreCommerce/ClientApp/src/app/app.module.ts
@@ -10,13 +10,16 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+import { ProvedoresComponent } from "./provedores/ProvedoresComponent";
+
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ProvedoresComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +29,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'provedores', component: ProvedoresComponent }
     ])
   ],
   providers: [],
---
+++ StoreCommerce/ClientApp/src/app/provedores/ProvedoresComponent.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'provedores-app',
+    templateUrl: './provedores.component.html'
+})
+export class ProvedoresComponent {
+    public nombre = "Hola mundo";
+}
