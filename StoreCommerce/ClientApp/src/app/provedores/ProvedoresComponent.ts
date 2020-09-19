77410af28e22a64238c834dd15f80f3b6ab645fe
EmmanuelBazan <mqkz99@gmail.com>
2020-09-18 22:27:50 -0500

    Creacion de formulario provedores

--- StoreCommerce/ClientApp/src/app/provedores/ProvedoresComponent.ts
+++ StoreCommerce/ClientApp/src/app/provedores/ProvedoresComponent.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'provedores-app',
-    templateUrl: './provedores.component.html'
+  selector: 'provedores-app',
+  templateUrl: './provedores.component.html',
+  styleUrls: ['./provedores.component.css']
 })
 export class ProvedoresComponent {
-    public nombre = "Hola mundo";
+    public nombre = "Provedores";
 }
