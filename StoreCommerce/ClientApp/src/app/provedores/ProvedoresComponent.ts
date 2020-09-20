41772bd1296a01c59041e34f7527e259b74a185e
EmmanuelBazan <mqkz99@gmail.com>
2020-09-20 00:13:52 -0500

    Test de conexion entre controller y angular

--- StoreCommerce/ClientApp/src/app/provedores/ProvedoresComponent.ts
+++ StoreCommerce/ClientApp/src/app/provedores/ProvedoresComponent.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'provedores-app',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./provedores.component.css']
 })
 export class ProvedoresComponent {
-    public nombre = "Provedores";
+  public nombre: string;
+  public proveedor: Proveedor[];
+  constructor(http: HttpClient, @Inject("BASE_URL") baseUrl: string) {
+    http.get<Proveedor[]>(baseUrl + "api/Povedor/ProvedorList").subscribe(result => {
+      this.proveedor = result;
+      this.nombre = this.proveedor[1].Proveedor1;
+      console.log("this.proveedor");
+      console.log(this.proveedor);
+    }, error => console.error(error));
+  }
+}
+
+interface Proveedor {
+  Idproveedor: number,
+  Proveedor1: string,
+  Telefono: number,
+  Direccion: string
 }
---
+++ StoreCommerce/Controllers/ProvedorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreCommerce.Models;
+
+namespace StoreCommerce.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProvedorController : Controller
+    {
+        private Models.storecommerceContext db = new Models.storecommerceContext();
+        public ProvedorController()
+        {
+            
+        }
+        [HttpGet("[action]")]
+        public IEnumerable<Proveedor> ProvedoresList()
+        {
+            List<Proveedor> proveedores = (from d in db.Proveedor
+                                           select new Proveedor
+                                           {
+                                               Idproveedor = d.Idproveedor,
+                                               Proveedor1 = d.Proveedor1,
+                                               Telefono = d.Telefono,
+                                               Direccion = d.Direccion
+                                           }).ToList();
+            return proveedores;
+        }
+    }
+}
